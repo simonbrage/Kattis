e029d1d90de9af423634e9137ff9ea1ab4516dff
Simon Brage <simonbrage@live.dk>
2022-09-01 23:02:30 +0200

    A different problem solved

---
+++ ADifferentProblem/Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {  
+    public static void main(String args[]) { 
+        Scanner s = new Scanner(System.in);
+        
+        while(s.hasNext()){
+            long a = s.nextLong();
+            long b = s.nextLong();
+        
+            long result = Math.abs(a - b);
+            System.out.println(result);
+            
+        }
+        
+        s.close();
+        
+    }
+}  
