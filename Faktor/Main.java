ccca9fce8b919f3b5c0f0bc95935c054c6cabb15
Simon Brage <simonbrage@live.dk>
2022-09-10 15:55:22 +0200

    Solved

---
+++ Faktor/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int a = s.nextInt();
+        int i = s.nextInt();
+
+        System.out.println((a*(i-1))+1);
+
+    }
+}
