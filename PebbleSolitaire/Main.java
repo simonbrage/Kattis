c21135efc84302934cd5855b93c0b2eda1b2238a
Simon Brage <simonbrage@live.dk>
2022-09-29 23:50:54 +0200

    Pebble Solitaire skeleton

---
+++ PebbleSolitaire/Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+
+    public static HashMap<String, Integer> hmap;
+    public static void main(String[] args)
+    {
+        Scanner s = new Scanner(System.in);
+
+        int n = s.nextInt();
+
+        for(int i = 0; i < n; i++) {
+            hmap = new HashMap<>();
+            StringBuilder initBoard = new StringBuilder(s.next());
+            // do something
+            System.out.println(/* result */);
+        }
+        
+        s.close();
+    }
+}
