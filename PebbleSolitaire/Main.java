100da7e259ee5b2165ae2baf86dba1ffc0dce7f8
Simon Brage <simonbrage@live.dk>
2022-09-30 16:30:24 +0200

    Solved

--- PebbleSolitaire/Main.java
+++ PebbleSolitaire/Main.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Main {
 
-    public static HashMap<String, Integer> hmap;
+    public static HashMap<String, Integer> memo;
     public static void main(String[] args)
     {
         Scanner s = new Scanner(System.in);
@@ -10,7 +10,7 @@ public class Main {
         int n = s.nextInt();
 
         for(int i = 0; i < n; i++) {
-            hmap = new HashMap<>();
+            memo = new HashMap<>();
             StringBuilder initBoard = new StringBuilder(s.next());
             int score = play(initBoard);
             System.out.println(score);
@@ -20,7 +20,7 @@ public class Main {
     }
 
     public static int play(StringBuilder board) {
-        if(hmap.containsKey(board.toString())) return hmap.get(board.toString());
+        if(memo.containsKey(board.toString())) return memo.get(board.toString());
 
         int score = 0;
 
@@ -28,15 +28,50 @@ public class Main {
             if(board.charAt(i) == 'o') score++;
         }
 
-        for(int i = 0; i < board.length(); i++) {
-            // do something
+        // Find moves to the right from bottom-up 
+        for(int i = 0; i < board.length() - 2; i++) {
+            if(board.charAt(i) == 'o' 
+                && board.charAt(i+1) == 'o' 
+                && board.charAt(i+2) == '-') {
+                    board.setCharAt(i, '-');
+                    board.setCharAt(i+1, '-');
+                    board.setCharAt(i+2, 'o');
+
+                    int newScore = play(board);
+                    if(newScore < score) {
+                        score = newScore;
+                    }
+
+                    // Reset
+                    board.setCharAt(i, 'o');
+                    board.setCharAt(i+1, 'o');
+                    board.setCharAt(i+2, '-');
+            }
         }
 
+        // Find moves to the left from top-down 
         for(int i = board.length() - 1; i >= 2; i--) {
-            // do something
+            if(board.charAt(i) == 'o' 
+            && board.charAt(i-1) == 'o' 
+            && board.charAt(i-2) == '-') {
+                board.setCharAt(i, '-');
+                board.setCharAt(i-1, '-');
+                board.setCharAt(i-2, 'o');
+                
+                int newScore = play(board);
+                if(newScore < score) {
+                    score = newScore;
+                }
+
+                // Reset
+                board.setCharAt(i, 'o');
+                board.setCharAt(i-1, 'o');
+                board.setCharAt(i-2, '-');
+        }
         }
 
-        hmap.put(board.toString(), score);
+        // Save board state (memoization)
+        memo.put(board.toString(), score);
         return score;
     }
 }
