bd265de34072bab5fc8b7550cc003a7a1dc60823
Simon Brage <simonbrage@live.dk>
2022-09-30 16:18:27 +0200

    Extend skeleton

--- PebbleSolitaire/Main.java
+++ PebbleSolitaire/Main.java
@@ -12,10 +12,31 @@ public class Main {
         for(int i = 0; i < n; i++) {
             hmap = new HashMap<>();
             StringBuilder initBoard = new StringBuilder(s.next());
-            // do something
-            System.out.println(/* result */);
+            int score = play(initBoard);
+            System.out.println(score);
         }
         
         s.close();
+    }
+
+    public static int play(StringBuilder board) {
+        if(hmap.containsKey(board.toString())) return hmap.get(board.toString());
+
+        int score = 0;
+
+        for(var i = 0; i < board.length(); i++) {
+            if(board.charAt(i) == 'o') score++;
+        }
+
+        for(int i = 0; i < board.length(); i++) {
+            // do something
+        }
+
+        for(int i = board.length() - 1; i >= 2; i--) {
+            // do something
+        }
+
+        hmap.put(board.toString(), score);
+        return score;
     }
 }
