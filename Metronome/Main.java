217d95756998700806b9e9644efaf0459c67e813
Simon Brage <simonbrage@live.dk>
2023-06-07 23:30:10 +0200

    Solved

---
+++ Metronome/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.util.Map.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int n = s.nextInt();
+        double turns = n / 4.00;
+
+        System.out.println(turns);
+    }
+}
